7947187ab83ac3d2210b40e3862c552da293f204
Zichead <nedomexo@gmail.com>
2023-08-29 20:13:45 +0300

    The models folder has been created and the Car class with three fields has been created in it

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Car {
+
+    private String model;
+
+    private int series;
+
+    private int yearOfRelease;
+
+    public Car(String model, int series, int yearOfRelease) {
+        this.model = model;
+        this.series = series;
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public void setYearOfRelease(int yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
+}
