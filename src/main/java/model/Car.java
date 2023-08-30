1ce4782443203d7acef79816dacd40e2af88b110
Zichead <nedomexo@gmail.com>
2023-08-30 15:43:48 +0300

    Added redefinition of equals and hashcode

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Objects;
+
 public class Car {
 
     private String model;
@@ -36,5 +38,25 @@ public class Car {
 
     public void setYearOfRelease(int yearOfRelease) {
         this.yearOfRelease = yearOfRelease;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Car car = (Car) o;
+
+        if (series != car.series) return false;
+        if (yearOfRelease != car.yearOfRelease) return false;
+        return Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = model != null ? model.hashCode() : 0;
+        result = 31 * result + series;
+        result = 31 * result + yearOfRelease;
+        return result;
     }
 }
