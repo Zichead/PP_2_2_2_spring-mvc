11d015925222c4fadb48bfd61f626689e07e9b03
Zichead <nedomexo@gmail.com>
2023-09-11 19:34:35 +0300

    Deleted RequestMapping

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,17 +1,18 @@
 package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import service.CarService;
 import service.impl.CarServiceImp;
 
 @Controller
-@RequestMapping("/cars")
 public class CarController {
 
 
     CarService carService = new CarServiceImp();
+
+    @GetMapping("/cars")
     public String getCars (@RequestParam ( value = "count", defaultValue = "5")  int count, ModelMap model) {
 
         model.addAttribute("cars", carService.getCarsToIndex(count));
