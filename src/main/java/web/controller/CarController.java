954a9394c9f837c0ff591d8b24a32a40b7d3d519
Zichead <nedomexo@gmail.com>
2023-09-15 07:24:02 +0300

    The project structure has been changed

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,8 +3,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import service.CarService;
-import service.impl.CarServiceImp;
+import web.service.CarService;
+import web.service.impl.CarServiceImp;
 
 @Controller
 public class CarController {
--- src/main/java/web/service/impl/CarServiceImp.java
+++ src/main/java/web/service/impl/CarServiceImp.java
@@ -1,26 +1,26 @@
-package service.impl;
+package web.service.impl;
 import web.model.Car;
 import org.springframework.stereotype.Component;
-import service.CarService;
+import web.service.CarService;
 
 import java.util.ArrayList;
 import java.util.List;
 @Component
 public class CarServiceImp implements CarService {
 
-    private final List<Car> carsList;
+    private final List<Car> carsList = new ArrayList<>();
 
     public CarServiceImp() {
-        carsList = new ArrayList<>();
-
-        carsList.add((new Car("Model1", 1, 2010)));
-        carsList.add((new Car("Model2", 2, 2011)));
-        carsList.add((new Car("Model3", 3, 2012)));
-        carsList.add((new Car("Model4", 4, 2023)));
-        carsList.add((new Car("Model5", 5, 2014)));
+        carsList.add(new Car("Model1", 1, 2010));
+        carsList.add(new Car("Model2", 2, 2011));
+        carsList.add(new Car("Model3", 3, 2012));
+        carsList.add(new Car("Model4", 4, 2023));
+        carsList.add(new Car("Model5", 5, 2014));
     }
+
     @Override
     public List<Car> getCarsToIndex(int index) {
+
         if (index>0) {
             return carsList.stream().limit(index).toList();
         } else {
